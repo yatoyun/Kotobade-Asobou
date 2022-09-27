e404321e99b567e615637f3e9a863ba3e0c165be
yatoyun <84168999+yatoyun@users.noreply.github.com>
2022-09-27 16:53:33 +0900

    Update wordle_play_real.py

--- wordle_play_real.py
+++ wordle_play_real.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import matplotlib.pyplot as plt
 import random
 import numpy as np
@@ -5,13 +6,20 @@ import argparse
 import copy
 import statistics
 import math
+from tqdm import tqdm
+
+from  concurrent import futures
+
+from hash import *
+from cachetools.func import lru_cache
+
 
 import time
 #from extract_for_wordle import extract_wordlist
 
-
 parser = argparse.ArgumentParser(description='For test and so on')
 
+@hashable_cache(lru_cache())
 #Create a list of possible correct words
 def extract_wordlist(try_wordlist, f, word):
     for i, a in enumerate(f):
@@ -31,7 +39,31 @@ def extract_wordlist(try_wordlist, f, word):
 
     return try_wordlist
 
+@hashable_cache(lru_cache())
+def get_next_word(try_wordlist):
+    min_len = 1e10
+    for word_to_guess in try_wordlist:
+        temp_eval_to_words_map = {}
+        for possible_answer in try_wordlist:
+            evaluation = tuple(return_find(possible_answer,[2,2,2,2], word_to_guess))
+
+            # store word by evaluation tuple in a list
+            if tuple(evaluation) not in temp_eval_to_words_map:
+                temp_eval_to_words_map[tuple(evaluation)] = [possible_answer]
+            else:
+                temp_eval_to_words_map[tuple(evaluation)].append(possible_answer)
+
+        # metric we are trying to minimize
+        biggest_possible_remaining_wordcount = max([len(val) for val in temp_eval_to_words_map.values()])
+
+        # if we found a new minimum
+        if biggest_possible_remaining_wordcount < min_len:
+            min_len = biggest_possible_remaining_wordcount
+            try_word = word_to_guess
+    return min_len, try_word
+
 #find the word list that contains hiragana that was yellow last time
+@hashable_cache(lru_cache())
 def find_word(word, find, find_word_list):
     for i, (w, b) in enumerate(zip(word, find)):
         if b == 1:
@@ -61,20 +93,21 @@ def return_find(word, find, correct):
     return find
 
 # recursive function
-def test(find, ex_word, try_wordlist):
+def test(find, ex_word, try_wordlist, correct_word):
     #check finish or not
-    if np.sum(find) == 0:
+    if sum(find) == 0:
         if not use_eval:
             print("Congratulation!!!")
         return 0
 
 
     # Not a single letter has yet been found.
-    if np.sum(find) == 2*4:
+    if sum(find) == 2*4:
         if ex_word != []:
             try_wordlist = extract_wordlist(try_wordlist, find, ex_word)
         num = len(try_wordlist)
-        index = random.randint(0,num-1)
+        if not use_super:
+            index = random.randint(0,num-1)
 
     else: # already at least one letter has been found
         try_wordlist = extract_wordlist(try_wordlist, find, ex_word)
@@ -82,11 +115,14 @@ def test(find, ex_word, try_wordlist):
         num = len(try_wordlist)
         if num == 0:
             print("len Error")
-            exit(2)
+            print(first_word, correct_word, find)
+            exit(0)
+
         elif num == 1:
             index = 0
         else:
-            index = random.randint(0, num-1)
+            if not use_super:
+                index = random.randint(0, num-1)
     if not use_eval:
         print("lest of vocab", num)
 
@@ -95,7 +131,7 @@ def test(find, ex_word, try_wordlist):
     if use_input:
         while True:
             print("input try word : ")
-            try_word = input()
+            try_word = list(input())
             if try_word == "list":
                 print(try_wordlist)
                 continue
@@ -109,7 +145,7 @@ def test(find, ex_word, try_wordlist):
             min_len = 1e12
             if num == num_valid_wordlist: #first time
                 if args.cheat:
-                    try_word = list("かいうん")
+                    try_word = list("はたして")#list("かいうん")
                 else:
                     try_word = list("れんぱつ")
             else: #except first time
@@ -120,24 +156,7 @@ def test(find, ex_word, try_wordlist):
                     if try_find[i] == 1:
                         try_find[i] = 2
 """
-                for word_to_guess in try_wordlist:
-                    temp_eval_to_words_map = {}
-                    for possible_answer in try_wordlist:
-                        evaluation = tuple(return_find(possible_answer,[2,2,2,2], word_to_guess))
-
-                        # store word by evaluation tuple in a list
-                        if tuple(evaluation) not in temp_eval_to_words_map:
-                            temp_eval_to_words_map[tuple(evaluation)] = [possible_answer]
-                        else:
-                            temp_eval_to_words_map[tuple(evaluation)].append(possible_answer)
-
-                    # metric we are trying to minimize
-                    biggest_possible_remaining_wordcount = max([len(val) for val in temp_eval_to_words_map.values()])
-
-                    # if we found a new minimum
-                    if biggest_possible_remaining_wordcount < min_len:
-                        min_len = biggest_possible_remaining_wordcount
-                        try_word = word_to_guess
+                min_len, try_word = get_next_word(try_wordlist)
                 """
                 for try_few_word in try_wordlist:
                     try_len = len(extract_wordlist(copy.deepcopy(try_wordlist), try_find, try_few_word)) # the number of a list of correct answers
@@ -182,49 +201,72 @@ def test(find, ex_word, try_wordlist):
             print(ans)
     #usual case
     if use_test:
-        print("input correct : ")
-        find = list(input())
-        find = [int(a) for a in find]
+        while True:
+            print("input correct : ")
+            find = input()
+            if find == "-1":
+                print(try_wordlist)
+            find = list(find)
+            try:
+                find = [int(a) for a in find]
+            except:
+                print("input must be number")
+            if len(find) == 4:
+                break
+            else:
+                print("input must be 4 digits")
     else: # for evaluation
         find = return_find(try_word, find, correct_word)
         if not use_eval:
             print(find)
 
-    return test(find, try_word, try_wordlist)+1
+    return test(find, try_word, try_wordlist, correct_word)+1
+
+def for_thread(find, ex_word, try_wordlist, correct_word, progress):
+    global faild
+    global fail_word
+
+    prob = test(find, ex_word, try_wordlist, correct_word)
+    if prob > 12:
+        faild += 1
+        fail_word.append(correct_word)
+    if prob == 1:
+        print("WoW first")
+    return prob
 
 
 def main():
     avg_prob = 0
     prob_array = []
-    faild = 0
     #for evaluation
     if use_eval:
         global correct_word
+        global second_word
+        global num_l
+        
         print("input number of eval : ")
         N = int(input())
+        
         start = time.time()
-        for i in range(N):
-            #reset find
-            find = [2]*4
-            #decide correct word
-            #correct_word = wordlist[random.randint(0,num_wordlist-1)]
-            correct_word = wordlist[i]
-            #print("Correct word : ",correct_word)
-            #prob
-            prob = test(find, [], valid_wordlists)
-            if prob > 12:
-                faild += 1
-                fail_word.append(correct_word)
+        future_list = []
+        with futures.ThreadPoolExecutor() as executor:
+            with tqdm(total=N) as progress:
+                for i in range(N):
+                    #reset find
+                    find = [2]*4
+                    #decide correct word
+                    #correct_word = wordlist[random.randint(0,num_wordlist-1)]
+                    correct_word = wordlist[i]
+                    #print("Correct word : ",correct_word)
+                    #prob
+                    future = executor.submit(for_thread, find, [], copy.deepcopy(valid_wordlists), correct_word, progress)
+                    future.add_done_callback(lambda p: progress.update())
+                    future_list.append(future)
+                for future in futures.as_completed(future_list):
+                    prob = future.result()
+                    avg_prob += prob
+                    prob_array.append(prob)
 
-            prob_array.append(prob)
-            avg_prob += prob
-            if prob == 1:
-                print("WoW first")
-            if N > 9:
-                if i%int(N/10) == 0:
-                    print(i+1, "回 | avg:", avg_prob/(i+1), ", avg faild:", faild/(i+1))
-            else:
-                print(i+1, "回 | avg:", avg_prob/(i+1), ", avg faild:", faild/(i+1))
         print("Avg prob : ", avg_prob / N, ", Avg faild:", faild/N)
         end = time.time()
         print("total time : ",end - start)
@@ -240,17 +282,19 @@ def main():
         #print('最も度数の多い階級：(' + str(bins[mode_index]) + ',' + str(bins[mode_index+1]) + ')')
         # 最頻値
         print('最頻値：', statistics.mode(prob_array))
+        print("faild word list : ", fail_word)
 
     else :
         find = [2]*4
 
-        print("The number of try until finish : ",test(find, [], valid_wordlists))
+        print("The number of try until finish : ",test(find, [], valid_wordlists, correct_word))
 
 
 if __name__ == '__main__':
     use_test = True
     correct_word = []
     fail_word = []
+    faild = 0
     #parser
     parser.add_argument('-t', '--test', help='correct word', default = "")
     parser.add_argument('-i','--input', help='self-input', action = "store_true")
@@ -290,9 +334,7 @@ if __name__ == '__main__':
 
     del wordlists
     del valid_wordlist
-
-    #test
+    
+    error = 0
 
     main()
-    #print(fail_word)
-
